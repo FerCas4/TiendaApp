8ea37346ed14e5d63791bfdb2881c5ad323bdb8d
FerCas4 <fernandoalbertocc33@gmail.com>
2026-07-15 20:12:44 -0400

    feat: implementacion inicial CRUD vistas

--- Program.cs
+++ Program.cs
@@ -1,18 +1,17 @@
 using Microsoft.EntityFrameworkCore;
-using TiendaApp.Data; // Esto soluciona el error de ApplicationDbContext
+using TiendaApp.Data; 
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllersWithViews();
 
-// CONFIGURACIÓN DE POSTGRESQL (Puesta antes de builder.Build())
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -21,7 +20,6 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// En .NET 9+, MapStaticAssets reemplaza en parte a UseStaticFiles, pero para asegurar compatibilidad:
 app.UseStaticFiles();
 
 app.UseRouting();
@@ -30,6 +28,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Productos}/{action=Index}/{id?}"); // Redirige directamente a tu CRUD de Productos
+    pattern: "{controller=Productos}/{action=Index}/{id?}"); 
 
 app.Run();
